cbeb7a573c4db5b39b75a2c88937c5af7fab1914
Boc@s <hodboc@gmail.com>
2026-08-01 08:39:30 +0100

    connexion nodejs à postgresql

--- leçon-12/cours/server.js
+++ leçon-12/cours/server.js
@@ -1,1 +1,73 @@
-const {pool}=require("pg");
+const express = require("express");
+const {Pool}=require("pg");
+const app=express();
+const PORT=3000;
+app.use(express.json());
+
+app.post("/api/inscription", async(req,res) =>{
+    /* console.log(req.body);
+    res.send({
+        message: "utilisé enregistré avec succès"
+    })
+   */ 
+  {
+    try {
+        const {nom,code}=req.body;
+        const resultat=await pool.query(
+              `
+                    INSERT INTO utilisateurs
+                    (nom, code_parrainage)
+                    VALUES ($1, $2)
+                    RETURNING *
+                    `,
+
+                    [
+                        nom,
+                        codeParrainage
+                    ]
+
+                );
+                res.status(201).json({
+                    message: "Utilisateur inscrit avec succès",
+                    utilisateur: resultat.rows[0]
+                });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Erreur lors de l'inscription"
+        });
+        
+    }
+  }
+})
+
+const pool = new Pool({
+
+    user: "postgres",
+
+    host: "localhost",
+
+    database: "plateforme_parrainage",
+
+    password: "@dmin051290",
+
+    port: 5432
+
+});
+pool.query("")
+    .then((resultat) => {
+
+        console.log(
+            "Connexion réussie :",
+            resultat.rows[0]
+        );
+
+    })
+    .catch((error) => {
+
+        console.log(
+            "Erreur de connexion :",
+            error
+        );
+
+    });
